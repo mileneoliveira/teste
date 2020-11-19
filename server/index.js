0712fa6ef4f4d58511fb15c698863aee3186eca9
mileneoliveira <milene.barbosa@bandtec.com.br>
2020-11-19 19:49:13 -0300

    Teste

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const msg = "API RODANDO";
+const msg = "API RO3DANDO";
 app.use(cors());
 app.get("/api",(_,res)=>{res.json({data:msg})});
 app.listen(process.env.PORT||3333,()=>console.log(msg));
